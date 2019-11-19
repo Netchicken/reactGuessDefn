9069930b164081a2aa102d238b3df5a514aabe8d
Gary Dix <intelproof@gmail.com>
2019-11-20 09:48:45 +1300

    Updates

---
+++ precache-manifest.52f1d0c2f16cd8f1f158aa670f2c263a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1601f225ad89942f5d17c16798027568",
+    "url": "/reactGuessDefn/index.html"
+  },
+  {
+    "revision": "01fddb68fc731d8d02b9",
+    "url": "/reactGuessDefn/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "72ecf7ce06f72c2bb575",
+    "url": "/reactGuessDefn/static/css/main.cb77e28f.chunk.css"
+  },
+  {
+    "revision": "01fddb68fc731d8d02b9",
+    "url": "/reactGuessDefn/static/js/2.aee1e530.chunk.js"
+  },
+  {
+    "revision": "72ecf7ce06f72c2bb575",
+    "url": "/reactGuessDefn/static/js/main.35ae2bc7.chunk.js"
+  },
+  {
+    "revision": "1fb2bdf0f512d06dc4de",
+    "url": "/reactGuessDefn/static/js/runtime-main.fa353b1f.js"
+  }
+]);
